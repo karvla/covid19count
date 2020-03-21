3d639aa97ca33d54ea45ffac37c0d05fd5e1db72
Erik Bjäreholt <erik@bjareho.lt>
2020-03-21 19:35:10 +0100

    major improvements, added support for deaths, added caching, etc.

--- covid19count.py
+++ covid19count.py
@@ -2,8 +2,10 @@
 
 import regex as re
 from datetime import datetime
-from typing import List
+from typing import List, Callable
+from pathlib import Path
 
+import pandas as pd
 import numpy as np
 import click
 import matplotlib.pyplot as plt
@@ -15,40 +17,81 @@ from bs4 import BeautifulSoup
 
 def _get_xls_url() -> str:
     url = "https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide"
-
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     xls_link_box = soup.find("a", href=re.compile(r".+\.xls"))
     return xls_link_box["href"]
 
 
-@click.command()
-@click.argument("regions", nargs=-1, required=True)
-@click.option("--log", is_flag=True)
-def plot_data(regions: List[str], log: bool):
-    import matplotlib.pyplot as plt
-    import pandas as pd
+def _download_file_cached(
+    get_url: Callable[[], str], path: Path, cache_expire: int = 300
+):
+    if (
+        not path.exists()
+        or path.lstat().st_mtime < datetime.now().timestamp() - cache_expire
+    ):
+        print("Downloading file...")
+        _download_file(get_url(), path)
+    else:
+        print("Used cache")
+    return path
 
-    # TODO: Make sure that XLS file is actually downloaded
-    df = pd.read_excel(
-        _get_xls_url(), index_col="DateRep", parse_dates=True
-    ).sort_index()
+
+def _download_file(url: str, path: Path) -> Path:
+    r = requests.get(url)
+    path.open("wb").write(r.content)
+    return path
+
+
+@click.group()
+def main():
+    """Tools to analyze cases and deaths from COVID-19"""
+    pass
+
+
+def _load_df() -> pd.DataFrame:
+    path = Path("data.xls")
+    _download_file_cached(_get_xls_url, path)
+    return pd.read_excel(path, index_col="DateRep", parse_dates=True).sort_index()
+
+
+@main.command()
+@click.argument("regions", nargs=-1, required=True)
+@click.option("--cum", is_flag=True, help="Plot cumulative cases/deaths")
+@click.option("--deaths", is_flag=True, help="Plot deaths")
+@click.option("--log", is_flag=True, help="Use a log scale on the y-axis")
+@click.option("--bar", is_flag=True, help="Use a bar plot instead")
+def plot(regions: List[str], cum: bool, deaths: bool, log: bool, bar: bool):
+    """Plot cases (by default) or deaths for different regions"""
+
+    cases_or_deaths = "Cases" if not deaths else "Deaths"
+    regions = [r.lower() for r in regions]
+
+    df = _load_df()
 
     for region in regions:
-        if region.lower() not in set(df["Countries and territories"].str.lower()):
+        if region not in set(df["Countries and territories"].str.lower()):
             print(f"Error: Region '{region}' does not exist in the dataset")
-            return
+            regions.remove(region)
 
-        df_r = df[df["Countries and territories"].str.lower() == region.lower()]
-        df_r = df_r["Cases"].cumsum()
-        # Remove rows before first case
-        df_r = df_r.truncate(before=df_r.ge(1).idxmax())
-        df_r.plot(logy=log, label=region)
+    df = df[df["Countries and territories"].str.lower().isin(regions)]
+    df = df.pivot(columns="Countries and territories", values=cases_or_deaths)
 
-    plt.ylabel("Number of confirmed cases")
-    plt.title(f"Confirmed cases per country as of {max(df.index).date()}")
+    # Remove rows before first case
+    df = df.truncate(before=df.sum(axis=1).ge(1).idxmax())
+    df = df.loc[: pd.Timestamp("2020-03-2")]
+    if cum:
+        df = df.cumsum()
+
+    plotf = df.plot if not bar else df.plot.bar
+    plotf(logy=log)
+
+    plt.ylabel(f"Number of new {cases_or_deaths}")
+    plt.title(f"New {cases_or_deaths} as of {max(df.index).date()}")
+    plt.ylim(0)
     plt.legend()
     plt.show()
 
+
 if __name__ == "__main__":
-    plot_data()
+    main()
